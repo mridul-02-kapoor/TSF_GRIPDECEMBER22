58405fa88cd0cbf2cab36f6063d5e3bd0080451f
Mridul Kapoor <112804648+mridul-02-kapoor@users.noreply.github.com>
2022-12-09 18:50:49 +0530

    Update Unsupervised ML.py

--- Task_2/Unsupervised ML.py
+++ Task_2/Unsupervised ML.py
@@ -10,25 +10,25 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 import seaborn as sns
 
-data_file = pd.read_csv("C:/Users/Mridul_Work/Desktop/TSF_GripDecember22_Mridul Kapoor/Task_2/Iris.csv") # reading file
-print("\nData imported successfully")                                                                    # to check whether data is imported or not
+data_file = pd.read_csv("C:/Users/Mridul_Work/Desktop/TSF_GripDecember22_Mridul Kapoor/Task_2/Iris.csv")        # reading file
+print("\nData imported successfully")                                                                           # to check whether data is imported or not
 
-print("\nFirst five rows\n",data_file.head())                                                            # print first five rows of collected data
+print("\nFirst five rows\n",data_file.head())                                                                   # print first five rows of collected data
 
-print("\nLast five rows\n",data_file.tail())                                                             # print last five rows of collected data
-
-
-data_file.isnull().sum()                                                                                # to check for null values or errors in dataset
+print("\nLast five rows\n",data_file.tail())                                                                    # print last five rows of collected data
 
 
-print(data_file.Species.nunique())                                                                      # to check for classes in given data
+data_file.isnull().sum()                                                                                        # to check for null values or errors in dataset
+
+
+print(data_file.Species.nunique())                                                                              # to check for classes in given data
 print(data_file.Species.value_counts())
 
 # DATA VISUALIZATION
 
 print("\nDot Plot")
 
-sns.set(style = 'whitegrid')                                                                            # Using Dot Plot
+sns.set(style = 'whitegrid')                                                                                    # Using Dot Plot
 dataset_iris = sns.load_dataset('iris')
 axis = sns.stripplot(x ='species',y = 'sepal_length',data = dataset_iris)
 
@@ -52,14 +52,14 @@ x = data_file.iloc[:,[0,1,2,3]].values
 from sklearn.cluster import KMeans
 wcss = []
 
-for i in range(1,11):                                                                                               # driving code
+for i in range(1,11):                                                                                             # driving code
     
     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
     kmeans.fit(x)
     
-    wcss.append(kmeans.inertia_)                                                                                    # appending the WCSS to the list (kmeans.inertia_ returns the WCSS value for an initialized cluster)
+    wcss.append(kmeans.inertia_)                                                                                   # appending the WCSS to the list (kmeans.inertia_ returns the WCSS value for an initialized cluster)
     
-    print("k: {} ; wcss: {}".format(i,kmeans.inertia_))                                                             # points generation
+    print("k: {} ; wcss: {}".format(i,kmeans.inertia_))                                                            # points generation
     
 
 # Plotting the results onto a line graph, allowing us to observe 'The elbow'
@@ -75,38 +75,38 @@ plt.show()
 
 #APPLYING K-MEANS -- K-MEANS CLASSFIER 
 
-kmeans = KMeans(n_clusters = 3, init = 'k-means++',max_iter = 300, n_init = 10, random_state = 0)                               # applying K-Means to the dataset 
+kmeans = KMeans(n_clusters = 3, init = 'k-means++',max_iter = 300, n_init = 10, random_state = 0)                     # applying K-Means to the dataset 
 
-y_kmeans = kmeans.fit_predict(x)                                                                                                # Returns a label for each data point based on the number of clusters
+y_kmeans = kmeans.fit_predict(x)                                                                                      # Returns a label for each data point based on the number of clusters
 
 
 # VISUALIZING THE CLUSTERS
 
 print("\nWithout Plotting the Centroids of each species")
 
-plt.figure(figsize=(15,8))                                                                                          # providing size of graph 
+plt.figure(figsize=(15,8))                                                                                              # providing size of graph 
 
-plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='yellow',label='Iris-setosa')                                 # dot plot of setosa
-plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='Iris-versicolour')                              # dot plot of versicolour
-plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='Iris-virginica')                               # dot plot of virginica
+plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='yellow',label='Iris-setosa')                                     # dot plot of setosa
+plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='Iris-versicolour')                                  # dot plot of versicolour
+plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='Iris-virginica')                                   # dot plot of virginica
 
-plt.grid(False)                                                                                                     # to remove gridlines
+plt.grid(False)                                                                                                         # to remove gridlines
 plt.show()
 
 ##Plotting the centroids of the clusters
 print("\n After Plotting the Centroids of all species (highlighted in red)")
 plt.figure(figsize=(15,8))
 
-plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='yellow',label='Iris-setosa')                                 # dot plot of setosa
-plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='Iris-versicolour')                              # dot plot of versicolour
-plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='Iris-virginica')                               # dot plot of virginica
+plt.scatter(x[y_kmeans==0,0],x[y_kmeans==0,1],s=100,c='yellow',label='Iris-setosa')                                     # dot plot of setosa
+plt.scatter(x[y_kmeans==1,0],x[y_kmeans==1,1],s=100,c='blue',label='Iris-versicolour')                                  # dot plot of versicolour
+plt.scatter(x[y_kmeans==2,0],x[y_kmeans==2,1],s=100,c='green',label='Iris-virginica')                                   # dot plot of virginica
 
-plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=100,c='red',label='Centroids')              # plotting dots on graph
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=100,c='red',label='Centroids')                  # plotting dots on graph
 
-plt.title('IRIS FLOWER CLUSTERS')                                                                                   # title of graph 
-plt.xlabel('SEPAL LENGTH (in cm)')                                                                                  # x axis
-plt.ylabel('PETAL LENGTH (in cm)')                                                                                  # y axis
-plt.legend()                                                                                                        # legend key
+plt.title('IRIS FLOWER CLUSTERS')                                                                                       # title of graph 
+plt.xlabel('SEPAL LENGTH (in cm)')                                                                                      # x axis
+plt.ylabel('PETAL LENGTH (in cm)')                                                                                      # y axis
+plt.legend()                                                                                                            # legend key
 
-plt.grid(False)                                                                                                     # to remove gridlines
+plt.grid(False)                                                                                                         # to remove gridlines
 plt.show()
